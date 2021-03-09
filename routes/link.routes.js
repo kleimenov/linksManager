ab13b9444434cb488cb6717b5ebab58986caf24f
Иван <kleimenov416@gmail.com>
2021-03-09 11:55:29 -0500

    start working at link route

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,4 +1,7 @@
-const {Router} =require('express');
-const Link = require('../models/Link');
+const { Router } = require("express");
+const { model } = require("mongoose");
+const Link = require("../models/Link");
 
 const router = Router();
+
+module.exports = router;
