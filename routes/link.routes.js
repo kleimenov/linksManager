e96b846fae0ef7e0b3e0327b5d9477661b929424
Иван <kleimenov416@gmail.com>
2021-03-09 11:59:11 -0500

    add couple of get and one post requests

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -3,4 +3,16 @@ const { model } = require("mongoose");
 const Link = require("../models/Link");
 const router = Router();
 
+router.post('/generate', async (req, res) => {
+
+})
+
+router.get('/', async (req, res) => {
+
+})
+
+router.get('/:id', async (req, res) => {
+    
+})
+
 module.exports = router;
