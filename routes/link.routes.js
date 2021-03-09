4b9858099a3feaaf11c04ac7a8d70448f760c51b
Иван <kleimenov416@gmail.com>
2021-03-09 12:02:34 -0500

    add try and catch and error message

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -3,16 +3,25 @@ const { model } = require("mongoose");
 const Link = require("../models/Link");
 const router = Router();
 
-router.post('/generate', async (req, res) => {
+router.post("/generate", async (req, res) => {
+  try {
+  } catch (e) {
+    res.status(500).json({ message: "Something goes wrong!" });
+  }
+});
 
-})
+router.get("/", async (req, res) => {
+    try {
+    } catch (e) {
+      res.status(500).json({ message: "Something goes wrong!" });
+    }
+});
 
-router.get('/', async (req, res) => {
-
-})
-
-router.get('/:id', async (req, res) => {
-    
-})
+router.get("/:id", async (req, res) => {
+    try {
+    } catch (e) {
+      res.status(500).json({ message: "Something goes wrong!" });
+    }
+});
 
 module.exports = router;
