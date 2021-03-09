c3b8308c196bf11b68c7791ba24787b35972de21
Иван <kleimenov416@gmail.com>
2021-03-09 16:12:36 -0500

    add spinner inside louder component

--- client/src/Pages/DetailsPage.js
+++ client/src/Pages/DetailsPage.js
@@ -1,7 +1,37 @@
-import React, { useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { useHttp } from "../hooks/http.hook";
+import {Loader} from '../components/Loader'
 
 export const DetailsPage = () => {
-  const [] = useState();
+  const { token } = useContext(AuthContext);
+  const { request, loading } = useHttp();
+  const [link, setLink] = useState(null);
+  const linkId = useParams().id;
+
+  const getLink = useCallback(
+    async () => {
+      try {
+        const fetched = await request(`/api/link/${linkId}`, "GET", null, {
+          Authorization: `Bearer ${token}`,
+        });
+        setLink(fetched);
+      } catch (e) {}
+    },
+    [token],
+    linkId,
+    request
+  );
+
+  useEffect(() => {
+      getLink()
+  }, [getLink])
+
+  if(loading) {
+      return <Loader />
+  }
+
   return (
     <div>
       <h3>Details page</h3>
