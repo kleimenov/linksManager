6f3b2b5741f56d89e4c2014103bb2229e2d4a3f3
Иван <kleimenov416@gmail.com>
2021-03-09 12:08:00 -0500

    add links route middleware to app.js server side

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.json( {extended: true} ))
+app.use(express.json({ extended: true }));
 
-app.use('/api/auth', require('./routes/auth.routes'))
-app.use('/api/link', require('./routes/link.routes'))
-
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/link", require("./routes/link.routes"));
 
 const PORT = config.get("PORT") || 3001;
 
 const start = async () => {
   try {
     await mongoose.connect(config.get("mongoUri"), {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
     });
     app.listen(PORT, () =>
       console.log(`Server is listening PORT ${PORT}.......`)
@@ -27,8 +26,5 @@ const start = async () => {
     process.exit(1);
   }
 };
-
-
-
 
 start();
