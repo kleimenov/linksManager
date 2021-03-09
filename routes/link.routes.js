ff678da60cb35f0f109dae14cbc50b27fc17ad2b
Иван <kleimenov416@gmail.com>
2021-03-09 12:45:07 -0500

    add new package shortid

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,15 +1,29 @@
 const { Router } = require("express");
 const { model } = require("mongoose");
-const shortid = require('shortid');
+const shortid = require("shortid");
 const Link = require("../models/Link");
-const auth = require('../middleware/auth.middleware')
-const config = require('config')
+const auth = require("../middleware/auth.middleware");
+const config = require("config");
 const router = Router();
 
-router.post("/generate", async (req, res) => {
+router.post("/generate", auth, async (req, res) => {
   try {
-      const baseUrl = config.get('baseUrl');
-      const {from} =req.body
+    const baseUrl = config.get("baseUrl");
+    const { from } = req.body;
+    const code = shortid.generate();
+
+    const existing = await Link.findOne({ from });
+
+    if (existing) {
+      return res.json({ link: existing });
+    }
+
+    const to = baseUrl + '/t/' + code;
+
+    const link = new Link({
+        code, to, from, owher: req.user.userId
+    })
+
   } catch (e) {
     res.status(500).json({ message: "Something goes wrong!" });
   }
@@ -24,7 +38,7 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
   try {
     const link = await Link.findById(req.params.id);
     res.json(link);
