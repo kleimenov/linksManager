6ac56784daa07b01e8a98f1d66f9fec0bcf0a944
Иван <kleimenov416@gmail.com>
2021-03-09 12:25:06 -0500

    continue working at request to DB, get response and build route logic

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,12 @@
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
+  try {
+      const token = req.headers.authorization.split(' ',)[1]
+      if(!token) {
+          res.status(401).json({message: 'does not authorized'})
+      }
+  } catch (e) {}
+ };
