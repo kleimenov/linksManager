5c8277e163914f1c5eea7f51b0d9bb84b70a014b
Иван <kleimenov416@gmail.com>
2021-03-10 11:06:46 -0500

    client side done

--- client/src/Pages/LinksPage.js
+++ client/src/Pages/LinksPage.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
-import {useHttp} from '../hooks/http.hook'
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { useHttp } from "../hooks/http.hook";
+import {AuthContext} from '../context/AuthContext'
+import { Loader } from "../components/Loader";
+import { LinksList } from "../components/LinksList";
 
 export const LinksPage = () => {
   const [links, setLinks] = useState([]);
-  const {loading, request} = useHttp();
+  const { loading, request } = useHttp();
+  const { token } = useContext(AuthContext);
+
+  const fetchLinks = useCallback(async () => {
+    try {
+      const fetched = await request("/api/link", "GET", null, {
+        Authorization: `Bearer ${token}`,
+      });
+      setLinks(fetched);
+    } catch (e) {}
+  }, [token, request]);
+
+  useEffect(() => {
+    fetchLinks();
+  }, [fetchLinks]);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
-    <div>
+    <>
       <h3>Links page</h3>
-    </div>
+      {!loading && <LinksList links={links} />}
+    </>
   );
 };
---
+++ client/src/components/LinksList.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+export const LinksList = ({ links }) => {
+  console.log(links);
+
+  if (!links.length) {
+    return <h4>You don't have any links</h4>;
+  }
+  return (
+    <div>
+      Hello
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Original link</th>
+            <th scope="col">Cut version</th>
+            <th scope="col">Open</th>
+          </tr>
+        </thead>
+        <tbody>
+          {links.map((link, i) => {
+            return (
+              <tr key={link._id}>
+                <th scope="row">{i + 1}</th>
+                <td scope="row">{link.from}</td>
+                <td scope="row">{link.to}</td>
+                <td>
+                  <Link to={`/details/${link._id}`}>Open</Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
